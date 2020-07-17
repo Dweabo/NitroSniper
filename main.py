1f418e63e72e66a58bdafa8523a8feb4d8604804
Adam <adamfyaffe@gmail.com>
2020-07-17 08:35:34 +0300

    hello

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
-from colorama import Fore, init, Style
-
-import urllib3
-import requests
-from discord.ext import commands
 import discord
+import asyncio
+from discord.ext import commands
+import re,requests
+from colorama import Fore, init
+import os
 
-
-client = commands.Bot(command_prefix='.', self_bot=True)
-init(autoreset=True, convert=True)
-client.remove_command('help')
-urllib3.disable_warnings()
-
+bot = commands.Bot(command_prefix='.', self_bot=True)
 token = 'mfa.kpTx9RILhvjZon7cikaohNRVrdbN63IE47g8My1UUGjYZr9oX7WTmlPyAXxw430uZpkiSxZ7vdImlW_Wfo_G'
-
-
-
-
-@client.event
+@bot.event
 async def on_connect():
-    print(Fore.GREEN+"=> Started FREESNIPER v2.0 by Dweeb")
-    print(Fore.WHITE+"=> Listening for Nitro Gifts")
+    print("ready")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="."))
 
-
-
-
-@client.event
+@bot.event
 async def on_message(message):
-    if 'https://discord.gift/' in message.content:
-        print(Fore.WHITE+"=> Found new Nitro Gift. Trying to claim it")
-        code = message.content.split('https://discord.gift')[1].split(' ')[0]
-        headers = {'Authorization': token, 'Content-Type': 'application/json', 'Accept': 'application/json'}
-        json = {
-            'channel_id': None,
-            'payment_source_id': None
-        }
-        r = requests.post('https://discordapp.com/api/v6/entitlements/gift-codes/'+code+'/redeem', verify=False,headers=headers, json=json)
-        if r.status_code == 200:
-            print(Fore.GREEN + "=> Successfully claimed Nitro with Code: "+code)
+    Userowner = bot.get_user(506489305997377536) # Your id discord
+    if message.content.startswith('https://discord.gift/'):
+        code = re.search("https://discord.gift/(.*)", message.content).group(1)
+        if len(code) != 16:
+            print("[X] fake code")
         else:
-            print(Fore.RED + "=> Code already claimed or not valid")
+            print(Fore.RED+"[+-] Snipped code : https://discord.gift/"+code+Fore.RESET)
+            r = requests
+            result = r.post('https://discordapp.com/api/v6/entitlements/gift-codes/'+code+'/redeem')
+            if 'This gift has been redeemed already.' in result:
+                print("[-] already reddem")
+            elif 'nitro' in result:
+                print(Fore.GREEN+"[+] found nitro : https://discord.gift/"+code+Fore.RESET)
+            elif 'Unknow Gift Code' in result:
+                print("[-] Invaild Code")
 
-client.run(token, bot=False)
+    if message.content.startswith('https://discordapp.com/gifts'):
+        codes = re.search("https://discordapp.com/gifts/(.*)", message.content).group(1)
+        if len(codes) != 16:
+            print("[X] fake code")
+        else:
+            print(Fore.RED+"[+-] Snipped code : https://discordapp.com/gifts/"+codes+Fore.RESET)
+            r = requests
+            result = r.post('https://discordapp.com/api/v6/entitlements/gift-codes/'+codes+'/redeem')
+            if 'This gift has been redeemed already.' in result:
+                print("[-] already reddem")
+            elif 'nitro' in result:
+                print(Fore.GREEN+"[+] found nitro : https://discordapp.com/gifts"+codes+Fore.RESET)
+            elif 'Unknow Gift Code' in result:
+                print("[-] Invaild Code")
+
+
+bot.run(token, bot=False)
