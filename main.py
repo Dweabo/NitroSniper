bba84b13308e1898a8ce374a9f2f118c204e8e4a
Adam <adamfyaffe@gmail.com>
2020-07-17 11:55:36 +0300

    hello

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = discord.Client()
 @client.event
 async def on_connect():
     print("ready")
+    await client.change_presence(activity=discord.Streaming(name=":3", url='https://www.twitch.tv/'))
 
 
 
